86aec0d42f08d05efa609a0ff81a8d5800abec8d
FretNot <dtwiltermood@mail.csuchico.edu>
2025-04-07 12:46:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RepoSetUp
+
+Developed with Unreal Engine 5
---
+++ Source/RepoSetUp/RepoSetUpGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RepoSetUpGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REPOSETUP_API ARepoSetUpGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
